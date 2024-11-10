2a07d634f531a8c667b217c0289afe74f899e605
digitribesthlm <patrik@makeablesthlm.se>
2024-11-10 20:22:20 +0100

    icons

--- pages/client/dashboard.js
+++ pages/client/dashboard.js
@@ -236,10 +236,32 @@ const AssetGroupsList = ({ assetGroups, onSelect }) => (
               <p className="text-sm text-base-content/70 line-clamp-3">{firstDescription}</p>
             </div>
 
+            {/* Asset Counts */}
+            <div className="flex flex-wrap gap-2 mt-4 text-sm text-base-content/60">
+              <div className="flex items-center gap-1 tooltip" data-tip="Headlines">
+                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z" />
+                </svg>
+                {group.headlines.length}
+              </div>
+              <div className="flex items-center gap-1 tooltip" data-tip="Images">
+                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                  <path fillRule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clipRule="evenodd" />
+                </svg>
+                {group.images.length}
+              </div>
+              <div className="flex items-center gap-1 tooltip" data-tip="Videos">
+                <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                </svg>
+                {group.videos?.length || 0}
+              </div>
+            </div>
+
             {/* Asset Group Name Button */}
             <button 
               onClick={() => onSelect(group)}
-              className="btn btn-primary w-full mt-auto"
+              className="btn btn-primary w-full mt-4"
             >
               {group.assetGroupName}
             </button>
